2cd9904e184ba75e9674daa2ef46241d61f5202a
Wush Wu <wush978@gmail.com>
2018-11-29 11:17:57 +0800

    add repo

--- tests/test-install.packages.via.graph.R
+++ tests/test-install.packages.via.graph.R
@@ -1,4 +1,13 @@
 library(pvm)
+R.date <- local({
+  m <- regexec("\\((.*)\\)$", R.version.string)
+  . <- regmatches(R.version.string, m)
+  . <- Filter(., f = function(.) length(.) == 2)
+  .[[1]][2]
+})
+R.date <- as.Date(R.date)
+repos <- c(CRAN = sprintf("https://cran.microsoft.com/snapshot/%s", R.date + 7))
+options(repos = repos)
 install.packages("Rcpp")
 install.packages.via.graph("xml2")
 stopifnot(require(xml2))
