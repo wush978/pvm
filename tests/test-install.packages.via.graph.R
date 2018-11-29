512f5aabdb4f3866528d83ed7681d2bdd8681c07
Wush Wu <wush978@gmail.com>
2018-11-29 13:01:46 +0800

    - debug: repos for R-devel

--- tests/test-install.packages.via.graph.R
+++ tests/test-install.packages.via.graph.R
@@ -1,7 +1,11 @@
 Sys.setenv("R_TESTS"="")
 library(pvm)
 R.date <- R.release.dates[sprintf("%s.%s", R.version$major, R.version$minor)]
-repos <- c(CRAN = sprintf("https://cran.microsoft.com/snapshot/%s", R.date + 7))
+if (is.na(R.date)) {
+  repos <- c(CRAN = "https://cloud.r-project.org/")
+} else {
+  repos <- c(CRAN = sprintf("https://cran.microsoft.com/snapshot/%s", R.date + 7))
+}
 options(repos = repos)
 install.packages("Rcpp")
 install.packages.via.graph("xml2")
